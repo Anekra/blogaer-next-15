bf4712ea59531b30392a6e2228753b924ca8f38c
anekra <andika.eka.102@gmail.com>
2024-11-07 18:27:14 +0700

    Chore: add some image urls.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,24 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         port: "",
         pathname: "/a/**"
+      },
+      {
+        protocol: "https",
+        hostname: "github.blog",
+        port: "",
+        pathname: "/wp-content/**"
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.cms-twdigitalassets.com",
+        port: "",
+        pathname: "/content/**"
+      },
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+        pathname: "/xz1dnu24egyd/**"
       }
     ]
   }
