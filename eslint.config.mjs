85156f96a7e90aa720bcc4bdbf1fc0c0059dfe58
anekra <andika.eka.102@gmail.com>
2024-12-02 14:08:57 +0700

    Chore: upgrade to eslint v9, remove is-url, add tailwindcss & eslint dev plugins, add webauthn.

---
+++ eslint.config.mjs
@@ -0,0 +1,86 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import _import from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+const config = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  {
+    plugins: {
+      import: _import,
+      "unused-imports": unusedImports
+    },
+    ignores: ["node_modules/", "dist/", "*.min.js", "**/slate.d.ts"],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object"
+          ],
+
+          "newlines-between": "always",
+
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after"
+            }
+          ],
+
+          pathGroupsExcludedImportTypes: ["builtin"],
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true
+          }
+        }
+      ],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_"
+        }
+      ]
+    }
+  },
+  {
+    files: ["**/*.mjs", "**/*.ts", "**/*.tsx", "**/*.css"],
+
+    rules: {
+      "no-undef": "off"
+    }
+  }
+];
+
+export default config;
