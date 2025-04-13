69813ae0b7a627446d0e19db82b39f6cde95b7b9
anekra <andika.eka.102@gmail.com>
2025-04-14 02:08:42 +0700

    Fix: undefined error by adding null check operator.

--- src/lib/components/sections/groups/SecurityGroup.tsx
+++ src/lib/components/sections/groups/SecurityGroup.tsx
@@ -6,19 +6,22 @@ import getClientFetch from "@/lib/actions/client/getClientFetch";
 import ChangePassSection from "@/lib/components/sections/settings/security/ChangePassSection";
 import OauthSection from "@/lib/components/sections/settings/security/OauthSection";
 import TwoFASection from "@/lib/components/sections/settings/security/TwoFASection";
-import { GetSecurityDto } from "@/lib/types/dto/GetDto";
+import { GetSecuritySectionDto } from "@/lib/types/dto/GetDto";
 
 export default function SecurityGroup() {
-  const { data: res } = useSWRImmutable<GetSecurityDto>(
+  const { data: res } = useSWRImmutable<GetSecuritySectionDto>(
     "/user/security",
     getClientFetch
   );
 
   return (
     <main className="grid w-full max-w-screen-2xl grid-cols-12 gap-8 px-6 py-4 lg:px-8">
-      <TwoFASection data={res?.data.userTwoFA} />
-      <ChangePassSection isPass={res?.data.userPassword} />
-      <OauthSection data={res?.data.userOauth} />
+      <TwoFASection data={res?.data?.userTwoFA} />
+      <ChangePassSection
+        isPassword={res?.data?.userPassword}
+        userRequest={res?.data?.userRequest}
+      />
+      <OauthSection data={res?.data?.userOauth} />
     </main>
   );
 }
