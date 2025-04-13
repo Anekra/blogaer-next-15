d9ce21bcde7572ab04fe8bfaec8ece2d7de4dcdd
anekra <andika.eka.102@gmail.com>
2025-04-14 02:02:12 +0700

    Dep-replace: remove shadcn toast and use sonner instead.

--- src/lib/components/forms/settings/account/DescriptionForm.tsx
+++ src/lib/components/forms/settings/account/DescriptionForm.tsx
@@ -1,32 +1,30 @@
 import { FormEvent } from "react";
+import { toast } from "sonner";
 
-import userPatchFetch from "@/lib/actions/server/userPatchFetch";
+import userPatch from "@/lib/actions/server/userPatch";
 import { Input } from "@/lib/components/ui/input";
 import { useSession } from "@/lib/contexts/SessionContext";
-import { useToast } from "@/lib/hooks/use-toast";
 
 export default function DescriptionForm() {
   const { session } = useSession();
-  const { toast } = useToast();
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const description = (e.currentTarget.firstChild as HTMLInputElement).value;
-    const response = await userPatchFetch(session, { description });
+    const response = await userPatch(session, { description });
+
     if (response.session) {
       localStorage.setItem(
         `${process.env.NEXT_PUBLIC_SESSION}`,
         response.session
       );
-      toast({
-        title: "Description updated successfully.",
-        duration: 2000,
-        variant: "success"
+      toast.success("Description updated successfully.", {
+        position: "bottom-right",
+        duration: 1500
       });
     } else {
-      toast({
-        title: response.error,
-        duration: 2000,
-        variant: "destructive"
+      toast.error(response.error, {
+        position: "bottom-right",
+        duration: 1500
       });
     }
   };
@@ -49,7 +47,7 @@ export default function DescriptionForm() {
         type="submit"
         className="btn-outline-base hidden !px-8 group-hover:block"
       >
-        {session?.desc ? "Edit" : "Add"}
+        {session?.desc !== "aaa" ? "Edit" : "Add"}
       </button>
     </form>
   );
