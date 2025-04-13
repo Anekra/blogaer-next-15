50ebebdae61cb2c77d4dc5aa689797a50cd953b8
anekra <andika.eka.102@gmail.com>
2025-04-14 02:12:02 +0700

    Feature: add request state to the component.

--- src/lib/components/sections/settings/security/ChangePassSection.tsx
+++ src/lib/components/sections/settings/security/ChangePassSection.tsx
@@ -1,19 +1,95 @@
-export default function ChangePassSection({ isPass }: { isPass?: boolean }) {
+import { FormEvent, useEffect, useState } from "react";
+
+import getClientFetch from "@/lib/actions/client/getClientFetch";
+import EmailStatusDialog from "@/lib/components/dialogs/EmailStatusDialog";
+import { DialogTrigger } from "@/lib/components/ui/dialog";
+import CountdownTimer from "@/lib/components/widgets/CountdownTimer";
+import { CommonDto, UserRequestDto } from "@/lib/types/dto/CommonDto";
+import { EmailInfo, EmailSubject } from "@/lib/utils/enums";
+
+export default function ChangePassSection({
+  isPassword,
+  userRequest
+}: {
+  isPassword?: boolean;
+  userRequest?: UserRequestDto;
+}) {
+  const [info, setInfo] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
+  const contents = [
+    isPassword ? "Request Reset Password" : "Request Add Password",
+    `An email containing a link to ${isPassword ? "reset your password" : "add a new password"} will sent your current email address. Are you sure you want to make this request?`,
+    "Request"
+  ];
+  const [timeLimit, setTimeLimit] = useState(userRequest?.limit);
+  const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setConfirmed(true);
+    const emailSubject = isPassword
+      ? EmailSubject.ResetPassword
+      : EmailSubject.AddPassword;
+    const resJson: CommonDto = await getClientFetch(
+      `/email/user/${emailSubject}`
+    );
+    if (resJson.data) setTimeLimit(resJson.data.limit);
+    resJson.error ? setInfo(resJson.error) : setInfo(EmailInfo.Success);
+  };
+
+  useEffect(() => {
+    if (userRequest?.limit) setTimeLimit(userRequest.limit);
+  }, [userRequest]);
+
   return (
     <section className="col-span-12 flex min-w-[380px] flex-col gap-2 rounded-lg ql:col-span-6">
-      <h2 className="text-lg font-bold">Change Password</h2>
-      <form className="glass-container flex flex-1 flex-col gap-6 p-6">
-        <p>
+      <h2 className="text-lg font-bold">
+        {isPassword ? "Change Password" : "Add New Password"}
+      </h2>
+      <div className="glass-container flex flex-1 p-6">
+        <div className="neu-base flex flex-col gap-6 rounded p-4">
+          <p>
+            {userRequest
+              ? "Email already been sent. Please check your email complete your password request process before the remaining time below ends."
+              : `
           An email will be sent to your registered email address. This email
-          will contain a link to create a new password or update your existing
-          one.
-        </p>
-        <div className="flex h-full items-center justify-center">
-          <button type="submit" className="btn-solid-p-rounder ql:p-6">
-            {isPass ? "Request change password" : "Request new password"}
-          </button>
+          will contain a link to
+          ${
+            isPassword
+              ? " update your existing password."
+              : " create a new password."
+          }
+          `}
+          </p>
+          <div className="flex h-full items-center justify-center">
+            {timeLimit ? (
+              <CountdownTimer timeLimit={timeLimit}>
+                <EmailStatusDialog
+                  contents={contents}
+                  confirmState={[confirmed, setConfirmed]}
+                  info={info}
+                  handleOnSubmit={handleOnSubmit}
+                >
+                  <DialogTrigger className="btn-solid-p-rounder">
+                    Request again
+                  </DialogTrigger>
+                </EmailStatusDialog>
+              </CountdownTimer>
+            ) : (
+              <EmailStatusDialog
+                contents={contents}
+                confirmState={[confirmed, setConfirmed]}
+                info={info}
+                handleOnSubmit={handleOnSubmit}
+              >
+                <DialogTrigger className="btn-solid-p-rounder p-6">
+                  {isPassword
+                    ? "Request change password"
+                    : "Request new password"}
+                </DialogTrigger>
+              </EmailStatusDialog>
+            )}
+          </div>
         </div>
-      </form>
+      </div>
     </section>
   );
 }
