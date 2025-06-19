6221ec5f293e506435bbc95e397fbe59c04be4d2
anekra <andika.eka.102@gmail.com>
2025-06-20 05:34:32 +0700

    Delete unused file.

--- src/app/blog/post/(nosidebar)/edit/[postTitle]/page.tsx
+++
@@ -1,9 +0,0 @@
-import PostEditorHolder from '@/lib/components/post/PostEditorHolder';
-
-export default function EditPostPage() {
-  return (
-    <main className="flex justify-center gap-6 pb-6 pt-24">
-      <PostEditorHolder />
-    </main>
-  );
-}
