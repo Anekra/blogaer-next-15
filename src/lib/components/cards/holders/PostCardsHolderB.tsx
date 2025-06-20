0e03e2372bde8086979ee2c0904f39ba3e1c83d2
anekra <andika.eka.102@gmail.com>
2025-06-20 14:58:23 +0700

    Improve: add condition for when the post data is empty.

--- src/lib/components/cards/holders/PostCardsHolderB.tsx
+++ src/lib/components/cards/holders/PostCardsHolderB.tsx
@@ -5,23 +5,24 @@ import useSWRImmutable from "swr/immutable";
 import getClientFetch from "@/lib/actions/client/getClientFetch";
 import PostGridCardC from "@/lib/components/cards/PostGridCardC";
 import { useCurrentPosts } from "@/lib/contexts/PostsContext";
-import { GetPostsByUserIdDto } from "@/lib/types/dto/GetDto";
+import { GetPostsByUserIdDto } from "@/lib/types/dto/ReqDto";
 
 export default function PostCardsHolderB({ url }: { url: string }) {
   const { currentPosts, setCurrentPosts } = useCurrentPosts();
-  const { data: res, error } = useSWRImmutable<GetPostsByUserIdDto>(
-    url,
-    (url: string) => getClientFetch(url)
-  );
+  const {
+    data: res,
+    error,
+    isLoading
+  } = useSWRImmutable<GetPostsByUserIdDto>(url, getClientFetch);
 
   useEffect(() => {
-    if (!res) return;
-    if (res.data.posts.length > 0) setCurrentPosts(res.data.posts);
+    if (res?.data && res.data.posts.length > 0) setCurrentPosts(res.data.posts);
   }, [res, setCurrentPosts]);
 
-  if (!res) return <p>loading...</p>;
+  if (!res || isLoading) return <p>loading...</p>;
   if (error) return <p>error</p>;
-  if (res.data.totalPosts === 0) return <p>No data found.</p>;
+  if (res.data?.totalPosts === 0) return <p>No data found!</p>;
+  if (res.error) return <p>{res.status}</p>;
 
   return (
     <div className="grid w-full auto-rows-fr grid-cols-1 gap-4 ms:grid-cols-2 ql:grid-cols-3">
