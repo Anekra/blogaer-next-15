bf20f0be8d83b8778c7040cc25587fea46668c5f
anekra <andika.eka.102@gmail.com>
2025-06-20 07:01:13 +0700

    Add id and text to the patch request body.

--- src/lib/components/buttons/SaveAndPublishBtn.tsx
+++ src/lib/components/buttons/SaveAndPublishBtn.tsx
@@ -2,24 +2,30 @@ import { toast } from "sonner";
 
 import patchClientFetch from "@/lib/actions/client/patchClientFetch";
 import { useContent } from "@/lib/contexts/ContentContext";
+import { serializeContent } from "@/lib/utils/helper";
 
 export default function SaveAndPublishBtn({ slug }: { slug: string }) {
   const { content, tags } = useContent();
   const title = `${content[0].children[0].text}`;
-  const handleEditPost = async () => {
-    const response = await patchClientFetch(
-      { title, content, tags },
-      `/post/${slug}`
-    );
-    if (response.message) {
-      toast.success(response.message, {
-        position: "bottom-right",
-        duration: 2000
-      });
+  const text = serializeContent(content);
+  const slugs = slug.split("-");
+  const id = slugs[slugs.length - 1];
+  const handleOnClick = async () => {
+    if (!title.includes(id)) {
+      const response = await patchClientFetch(
+        { id, title, text, content, tags },
+        `/post`
+      );
+      if (response.message) {
+        toast.success(response.message, {
+          position: "bottom-right",
+          duration: 2000
+        });
+      }
     }
   };
   return (
-    <button className="btn-solid-p" onClick={handleEditPost}>
+    <button className="btn-solid-p" onClick={handleOnClick}>
       Save & Publish
     </button>
   );
