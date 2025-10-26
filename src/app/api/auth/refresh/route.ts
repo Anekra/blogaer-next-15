4512a7f658ce5bf3980373ac40e8feaa02fd7bb0
anekra <andika.eka.102@gmail.com>
2025-10-26 14:55:56 +0700

    Fix: no longer use access & refresh token in the client.

--- src/app/api/auth/refresh/route.ts
+++ src/app/api/auth/refresh/route.ts
@@ -1,90 +1,45 @@
 import jwt from "jsonwebtoken";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-import { RefreshTokenJson, Session } from "@/lib/types";
+import { Session } from "@/lib/types";
+import setSessionCookie from "@/lib/actions/server/auth/setSessionCookie";
+import { cookies } from "next/headers";
 
-export async function GET(request: NextRequest) {
-  const accessCookieName = `${process.env.ACCESS_TOKEN}`;
-  const refreshCookieName = `${process.env.REFRESH_TOKEN}`;
-
-  const refreshToken = request.cookies.get(refreshCookieName)?.value;
-  const redirectRes = NextResponse.json(
-    { error: "Session token expired." },
-    { status: 419 }
-  );
-  if (!refreshToken) {
-    return redirectRes;
-  }
-
+export async function GET() {
   try {
+    const redirectRes = NextResponse.json(
+      { error: "Session token expired." },
+      { status: 419 }
+    );
+    const cookie = await cookies();
+    const sessionCookie = `${process.env.SESSION}`;
+    const encryptedSession = cookie.get(sessionCookie)?.value;
+    if (!encryptedSession) return redirectRes;
+
+    const decodedSession = jwt.decode(encryptedSession) as Session;
+    if (!decodedSession) return redirectRes;
+
     const url = `${process.env.API_ROUTE}/auth/refresh`;
     const refreshResponse = await fetch(url, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Origin: "http://localhost:3000",
-        Cookie: `${process.env.REFRESH_TOKEN}=${refreshToken}`
+        Origin: "http://localhost:3000"
       }
     });
-    if (!refreshResponse.ok) {
-      redirectRes.cookies.delete(accessCookieName);
-      redirectRes.cookies.delete(refreshCookieName);
-
-      return redirectRes;
-    }
-
-    const sessionToken = request.nextUrl.searchParams.get("session");
-    if (!sessionToken) {
-      return redirectRes;
-    }
-
-    const decodedSession = jwt.decode(sessionToken) as Session;
-    const stripedSession = { ...decodedSession };
-    stripedSession.exp = Date.now() / 1000 + 1 * 10 * 60;
-    const encryptedData = jwt.sign(stripedSession, `${process.env.SESSION}`);
-    const refreshJson: RefreshTokenJson = await refreshResponse.json();
+    if (!refreshResponse.ok) return redirectRes;
+    const refreshedSession = { ...decodedSession };
+    refreshedSession.exp = Date.now() / 1000 + 1 * 10 * 60;
     const response = NextResponse.json(
-      { session: encryptedData },
+      { exp: refreshedSession.exp },
       { status: 200 }
     );
-    const isSecure = process.env.NODE_ENV === "production";
-    // access
-    response.cookies.set(
-      accessCookieName,
-      refreshJson.data.access,
-      isSecure
-        ? {
-            httpOnly: true,
-            sameSite: "none",
-            secure: true,
-            maxAge: 1 * 10 * 60
-          }
-        : {
-            httpOnly: true,
-            maxAge: 1 * 10 * 60
-          }
-    );
-    // refresh
-    response.cookies.set(
-      refreshCookieName,
-      refreshJson.data.refresh,
-      isSecure
-        ? {
-            httpOnly: true,
-            sameSite: "lax",
-            secure: true,
-            maxAge: 24 * 60 * 60
-          }
-        : {
-            httpOnly: true,
-            maxAge: 24 * 60 * 60
-          }
-    );
-    console.log("<<< refresh token route S >>>");
+
+    await setSessionCookie(refreshedSession);
 
     return response;
   } catch (error) {
-    console.error("<<< refresh token error >>>", error);
+    console.error("GET /auth/refresh/route", error);
 
     return NextResponse.json(
       { message: "Server currently down." },
